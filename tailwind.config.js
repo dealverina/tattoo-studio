ebcf7b6346f71675142cd559efe7f17f30243c33
DEA ALVERINA <dealverina@gmail.com>
2024-07-29 22:20:19 +0700

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      primary: "Oswald",
+      secondary: "Raleway",
+      tertiary: "Rozha One",
+    },
+    container: {
+      padding: {
+        DEFAULT: "20px",
+        lg: "0",
+      },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1300px",
+    },
+    extend: {
+      colors: {
+        dark: "#111111",
+        grey: {
+          DEFAULT: "#555555",
+        },
+      },
+      backgroundImage: {
+        hero: "url('/src/assets/img/hero/bg.jpg')",
+        about: "url('/src/assets/img/about/image.png')",
+        interview: "url('/src/assets/img/interview/bg.png')",
+      },
+      content: {
+        outlineText: "url('/src/assets/img/hero/outline-text.svg')",
+      },
+    },
   },
   plugins: [],
-}
-
+};
