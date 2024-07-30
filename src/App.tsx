fcebbc7bfb5248d571762560cce18e0158fdb66f
DEA ALVERINA <dealverina@gmail.com>
2024-07-30 11:21:18 +0700

    adding components to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,29 @@
+import {
+  About,
+  Contact,
+  Copyright,
+  Footer,
+  GallerySection,
+  Header,
+  Hero,
+  Interview,
+  Skills,
+  Testimonial,
+} from "./components";
+
 export const App = () => {
-  return <>Hello</>;
+  return (
+    <div className="max-w-[1920px] mx-auto overflow-hidden bg-white">
+      <Header />
+      <Hero />
+      <About />
+      <GallerySection />
+      <Skills />
+      <Testimonial />
+      <Interview />
+      <Contact />
+      <Footer />
+      <Copyright />
+    </div>
+  );
 };
