0bcab28ab9eb014a4dbb6430843daf07e562da81
DEA ALVERINA <dealverina@gmail.com>
2024-08-04 14:53:14 +0700

    skill section

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,5 +1,56 @@
+import "react-circular-progressbar/dist/styles.css";
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
+import { CircularProgressbar } from "react-circular-progressbar";
+// import { motion } from "framer-motion";
+// import { fadeIn } from "../variants";
+
 export const Skills = () => {
+  const [fullBody, setFullBody] = useState(0);
+
+  const { ref, inView } = useInView({
+    threshold: 0.2,
+  });
+
+  const styles = {
+    path: {
+      stroke: "#111111",
+    },
+    trail: {
+      stroke: "#eeeeee",
+    },
+    text: {
+      fill: "#111111",
+      fontSize: "24px",
+    },
+  };
+
+  useEffect(() => {
+    if (inView) {
+      setTimeout(() => {
+        if (fullBody < 20) {
+          setFullBody(fullBody + 1);
+        }
+      }, 50);
+    } else {
+      setFullBody(0);
+    }
+  }, [fullBody, inView]);
+
   return (
-    <div>Skills</div>
-  )
-}
+    <section ref={ref} className="bg-pink-200 section">
+      <div className="container mx-auto">
+        <div>
+          <div>
+            <CircularProgressbar
+              strokeWidth={1}
+              value={fullBody}
+              styles={styles}
+              text={`${fullBody}%`}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
